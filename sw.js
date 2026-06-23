22a16002e936121371a441a38279952c335676fa
Adexmosho <Adexmoshocartradomedical@outlook.com>
2026-06-23 10:13:21 -0700

    Add files via upload
    update

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'adexmosho-cache-v3';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
